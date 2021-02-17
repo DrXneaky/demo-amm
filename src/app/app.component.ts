eadf50bdd98e41b1c42c1b665d375510b1c87488
AHMED MILADI <amiladi@3s.com.tn>
2021-02-17 11:06:02 +0100

    oidc works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,13 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable } from 'rxjs';
 
 export const authConfig2: AuthConfig = {
-  issuer: 'http://phenixid-test.francecentral.cloudapp.azure.com/dummy',
+  issuer: 'http://phenixid-test.francecentral.cloudapp.azure.com/sso2',
   strictDiscoveryDocumentValidation: false,
   redirectUri: window.location.origin,
   clientId: 'app1',
   dummyClientSecret: 'cisco123',
   responseType: 'code',
-  scope: 'openid',
+  scope: 'openid profile',
   disablePKCE: true,
   showDebugInformation: true,
   postLogoutRedirectUri: window.location.origin,
@@ -28,19 +28,17 @@ export const authConfig2: AuthConfig = {
 export class AppComponent {
   title = 'demo-app 2';
 
-  constructor(private oauthService: OAuthService, private http: HttpClient) {
+  constructor(public oauthService: OAuthService, private http: HttpClient) {
     this.oauthService.configure(authConfig2);
-    //this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
     this.oauthService.loadDiscoveryDocumentAndLogin().then(_ => {
       console.log("logged In");
     }).catch(err => {
-      console.log("Unable to login");
-      console.log(err);
     })
   }
 
   login() {
-    this.oauthService.initImplicitFlow();
+    this.oauthService.initImplicitFlow('foobar');
     //this.oauthService.initAuthorizationCodeFlow();
   }
 
@@ -53,51 +51,34 @@ export class AppComponent {
     if (!claims) {
       return null;
     }
-    //console.log('claims', claims);
-    return claims['sub'];
-    //return claims['first_name'];
+    return claims['given_name'];
   }
 
-  getInfo() {
-    //const info = this.oauthService.loadUserProfile();
+  getInfo(): String {
+    const info = this.oauthService.loadUserProfile();
+    console.log('this is info', info);
     const token = this.oauthService.getAccessToken();
-    console.log('token', token);
     const httpOptions = {
       headers: new HttpHeaders({
+        "Accept": "application/json",
         "Content-Type": "application/json",
         "Authorization": "Bearer " + token,
       }),
     };
     this.userInfo(httpOptions).subscribe(
       (data: any) => {
-        console.log('userinfo', data);
+        return data['given_name'];
+      }, (error) => {
+        return null;
       }
     );
+    return null;
     //console.log('info', info);
   }
 
   userInfo(httpOptions): Observable<any> {
     const uri = this.oauthService.userinfoEndpoint;
-    console.log('userInfoEndpoint', uri);
-    return this.http.post<any>(uri, "", httpOptions);
+    return this.http.get<any>(uri, httpOptions);
   }
 
-  /* exchancheCodeForToken(code){
-    const tokenEndpt = this.oauthService.tokenEndpoint;
-    console.log("token endpoint",tokenEndpt);
-    const uri = 'http://phenixid-test.francecentral.cloudapp.azure.com/api/authentication/cb169470-1440-4429-aa40-d3dd78320dfc?tenant=dummy&grant_type=authorization_code&code=' + code + '&redirect_uri=http://localhost:4200&client_id=app1&client_secret=cisco123';
-    return this.http.post<any>(uri, "");
-  } */
-
-  /* logToken(code) {
-    this.exchancheCodeForToken(code).subscribe(
-      (data: any) => {
-        console.log(data.access_token);
-        localStorage.setItem('access_token', data.access_token);
-        localStorage.setItem('id_token', data.id_token);
-      }, (error) => {
-        console.log(error);
-      }
-    )
-  } */
 }
--- src/app/shared/interceptor.ts
+++ src/app/shared/interceptor.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { OAuthService, AuthConfig } from 'angular-oauth2-oidc';
+import { AppComponent} from '../app.component';
+
 @Injectable()
 export class Interceptor implements HttpInterceptor {
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (request.url == 'http://phenixid-test.francecentral.cloudapp.azure.com/api/authentication/cb169470-1440-4429-aa40-d3dd78320dfc?tenant=dummy') {
-        console.log("type",typeof(request.body));
-        console.log("body",request.body.get('code'));
+        if (request.url == 'http://phenixid-test.francecentral.cloudapp.azure.com/api/authentication/b9bc1588-fdef-43eb-b866-329a42cbaa77?tenant=sso2') {
         const new_request = new HttpRequest('GET', request.url);
         const customReq = request.clone({});
         const customReqReplace = new_request.clone({
